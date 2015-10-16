7a551df6ad4f94fe7d05e8a58fdd2b350d438032
Joshua Paling <joshua.paling@gmail.com>
2015-10-17 00:04:06 +1100

    finish tutorial

---
+++ index.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('a user disconnected');
+    io.emit('user connected');
+  });
+  socket.on('chat message', function (msg) {
+    console.log('mesage: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});

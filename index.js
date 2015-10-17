fe8fcbe935ae533d0eb1672ab72aad8781713251
Joshua Paling <joshua.paling@gmail.com>
2015-10-17 16:51:40 +1100

    broadcast msg when user connects or disconnects

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  socket.broadcast.emit('user connected');
+
   socket.on('disconnect', function(){
-    console.log('a user disconnected');
-    io.emit('user connected');
+    io.emit('user disconnected');
   });
+
   socket.on('chat message', function (msg) {
-    console.log('mesage: ' + msg);
     io.emit('chat message', msg);
   });
 });
